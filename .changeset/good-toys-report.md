28820dde230721dc30d4f091ea7d718f2a13fbae
Jan Šimeček <simecekjann@gmail.com>
2024-02-27 01:00:02 +0100

    Fixed badge styles

---
+++ .changeset/good-toys-report.md
@@ -0,0 +1,5 @@
+---
+"@utima/ui": patch
+---
+
+Re-added inline-flex to badge
--- packages/ui/src/components/badge/Badge.styles.ts
+++ packages/ui/src/components/badge/Badge.styles.ts
@@ -6,7 +6,7 @@ const defaultBorder = 'bg-none bg-transparent border';
 
 export const badgeDef = twOverrides(
   {
-    badge: 'border rounded-radius font-semibold inline transition-all h-auto',
+    badge: 'border rounded-radius font-semibold inline-flex items-center transition-all h-auto',
     variants: {
       variant: {
         primary: 'bg-primary border-primary text-primary-fg',
