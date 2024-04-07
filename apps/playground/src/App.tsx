cbb28d4ad6a72032fd618b67a7e682efcb406851
Jan Šimeček <simecekjann@gmail.com>
2024-04-07 17:32:14 +0200

    Fixed lint

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -26,6 +26,71 @@ function App() {
       </Layout.Header>
       <Layout.Sidebar>
         <Menu.Root>
+          <Menu.Content>
+            <Menu.Item>
+              <Menu.Link>
+                <Home className='size-5' /> Home
+              </Menu.Link>
+            </Menu.Item>
+            <Menu.Separator />
+            <Menu.Label>Group 1</Menu.Label>
+            <Menu.Item>
+              <Menu.Link active>
+                <LayoutDashboard className='size-5' /> Dashboard
+              </Menu.Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Sub defaultOpen>
+                <Menu.SubTrigger>
+                  <Menu.Link>
+                    <Contact className='size-5' /> Contacts
+                  </Menu.Link>
+                </Menu.SubTrigger>
+                <Menu.SubContent>
+                  <Menu.SubItem>
+                    <Menu.Link>
+                      <User className='size-5' /> Profile
+                    </Menu.Link>
+                  </Menu.SubItem>
+                  <Menu.SubItem>
+                    <Menu.Link>
+                      <Lock className='size-5' /> Account
+                    </Menu.Link>
+                  </Menu.SubItem>
+                  <Menu.Separator />
+                  <Menu.SubItem>
+                    <Menu.Sub open={open} onOpenChange={setIsOpen}>
+                      <Menu.SubTrigger>
+                        <Menu.Link>Dashboard</Menu.Link>
+                      </Menu.SubTrigger>
+                      <Menu.SubContent>
+                        <Menu.Label>Group 1</Menu.Label>
+                        <Menu.SubItem>
+                          <Menu.Link indent>Profile</Menu.Link>
+                        </Menu.SubItem>
+                        <Menu.SubItem>
+                          <Menu.Link indent>Account</Menu.Link>
+                        </Menu.SubItem>
+                        <Menu.SubItem>
+                          <Menu.Link indent>Security</Menu.Link>
+                        </Menu.SubItem>
+                      </Menu.SubContent>
+                    </Menu.Sub>
+                  </Menu.SubItem>
+                </Menu.SubContent>
+              </Menu.Sub>
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Link>
+                <LogOut className='size-5' /> Logout
+              </Menu.Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Link>
+                <Cog className='size-5' /> Settings
+              </Menu.Link>
+            </Menu.Item>
+          </Menu.Content>
         </Menu.Root>
       </Layout.Sidebar>
       <Layout.Content>
