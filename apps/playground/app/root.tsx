8203779b8a5020562d836312694d5a70808238f8
Jan Šimeček <simecekjann@gmail.com>
2024-09-20 01:53:31 +0200

    Remix playground

--- apps/playground/app/root.tsx
+++ apps/playground/app/root.tsx
@@ -8,6 +8,8 @@ import {
 } from '@remix-run/react';
 
 import './tailwind.css';
+import { Button, Layout as UILayout } from '@utima/ui';
+import { Cloud, Home, Menu, Users } from 'lucide-react';
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -42,5 +44,48 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 // eslint-disable-next-line react/no-multi-comp
 export default function App() {
-  return <Outlet />;
+  return (
+    <UILayout.Root>
+      <UILayout.Header className='bg-slate-900 px-3 gap-3'>
+        <UILayout.SidebarTrigger>
+          <Button size='icon-sm' variant='secondary'>
+            <Menu />
+          </Button>
+        </UILayout.SidebarTrigger>
+
+        <h1 className='text-white text-lg font-bold'>Company LOGO</h1>
+      </UILayout.Header>
+      <UILayout.Sidebar className='bg-slate-700 p-2'>
+        <ol className='flex flex-col gap-2'>
+          <li>
+            <a
+              href='#'
+              className='flex gap-2 hover:bg-slate-800 p-2 rounded-radius'
+            >
+              <Home /> Home
+            </a>
+          </li>
+          <li>
+            <a
+              href='#'
+              className='flex gap-2 hover:bg-slate-800 p-2 rounded-radius'
+            >
+              <Users /> Users
+            </a>
+          </li>
+          <li>
+            <a
+              href='#'
+              className='flex gap-2 hover:bg-slate-800 p-2 rounded-radius'
+            >
+              <Cloud /> Dashboard
+            </a>
+          </li>
+        </ol>
+      </UILayout.Sidebar>
+      <UILayout.Content className='p-5'>
+        <Outlet />
+      </UILayout.Content>
+    </UILayout.Root>
+  );
 }
