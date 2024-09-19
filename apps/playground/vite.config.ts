f5d32af4e29b678c22a8d2e22693ed6cfcc28d9a
Jan Šimeček <simecekjann@gmail.com>
2024-09-20 01:42:27 +0200

    New playground

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,6 +1,16 @@
-import react from '@vitejs/plugin-react-swc';
+import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
